5e4831b39525319b79a8fd68a9faf3f91d22bf9d
Nick Aristizabal <99772297+NickAwrist@users.noreply.github.com>
2024-04-21 00:07:35 -0400

    Added paragraphs explaining the assignment, my approach, and revisions I made for the technical documents

--- src/pages/tempPages/InstructionsPage.tsx
+++ src/pages/tempPages/InstructionsPage.tsx
@@ -7,6 +7,26 @@ const InstructionsPage: React.FC = () => {
     return(
        <div>
             <h2>Hoyolab Bot Setup Instructions</h2>
+
+            <p>
+                These instructions were part of an assignment in my Technical Writing class (ENC 3241) at UCF. We were tasked to create a set of instructions
+                for a specific audience. In this case, the audience is people who are interested in setting up this Discord bot for their server.
+            </p>
+            <p>
+                I approached this assignment by breaking down the steps into smaler and more managaable tasks. 
+                I also took screenshots of the process to help the reader visualize what they should be seeing on their screen. I then 
+                organized the steps in a logical order, starting with the prerequisites, then the installation, and finally the setup.
+                Eacg step had a detailed description of what the user should do, and the screenshots were placed next to the 
+                steps they corresponded to.
+            </p>
+
+            <p>
+                My original submission had a few issues that I had to fix. Issues like having too large of spacing between some steps,
+                not elaborating enough on some steps, going over the 1000 word limit, and my screenshots not being of the highest quality.
+                In my revision, I fixed these issues by removing some unnecessary spacing, reworded some steps to be more concise,
+                broke some steps down into smaller steps, and replaced the screenshots with higher quality ones. Below is the final version of the instructions.
+            </p>
+
             <div className="pdf">
                 <object 
                 data="temp/Instructions.pdf" 
